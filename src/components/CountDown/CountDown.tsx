e80b7d1daa9cbd770c56ebc543de663059464370
Seungchan <scjin91@gmail.com>
2021-10-12 23:38:25 +0900

    Update CountDown Date

--- src/components/CountDown/CountDown.tsx
+++ src/components/CountDown/CountDown.tsx
@@ -8,9 +8,16 @@ const MINUTES = SECONDS * 60;
 const HOURS = MINUTES * 60;
 const DAYS = HOURS * 24;
 
-const CountDown = () => {
-  const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+function timeLeftUntilNextWeek() {
+  const time = new Date();
+  time.setDate(time.getDate() + (6 - time.getDay()));
+  time.setHours(23);
+  time.setMinutes(59);
+  time.setSeconds(59);
+  return time;
+}
 
+const CountDown = () => {
   const [days, setDays] = useState<number>(0);
   const [hours, setHours] = useState<number>(0);
   const [minutes, setMinutes] = useState<number>(0);
@@ -18,6 +25,8 @@ const CountDown = () => {
 
   // https://www.w3schools.com/howto/howto_js_countdown.asp
   useEffect(() => {
+    let countDownDate = timeLeftUntilNextWeek().getTime();
+
     setInterval(() => {
       const now = new Date().getTime();
       const distance = countDownDate - now;
@@ -26,8 +35,12 @@ const CountDown = () => {
       setHours(Math.floor((distance % DAYS) / HOURS));
       setMinutes(Math.floor((distance % HOURS) / MINUTES));
       setSeconds(Math.floor((distance % MINUTES) / SECONDS));
+
+      if (distance < 0) {
+        countDownDate = timeLeftUntilNextWeek().getTime();
+      }
     }, 1000);
-  }, [countDownDate]);
+  }, []);
 
   return (
     <S.Container>
